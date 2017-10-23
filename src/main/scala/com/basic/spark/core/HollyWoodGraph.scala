cd6d201bdb38d4b045520443c1821dcf1d7df998
Master.TJ <798750509@qq.com>
2017-10-23 16:00:43 +0800

    2017/10/23 16:00  modify...............

---
+++ src/main/scala/com/basic/spark/core/HollyWoodGraph.scala
@@ -0,0 +1,22 @@
+package com.basic.spark.core
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * locate com.basic.spark.core
+  * Created by 79875 on 2017/10/20.
+  */
+class HollyWoodGraph {
+    def main(args: Array[String]): Unit = {
+//        val conf = new SparkConf()
+//        conf.setAppName("analysis")
+//
+//        val sc = new SparkContext(conf)
+//        val data = sc.textFile(args(0))
+//
+//        data.cache
+//        data.flatMap(str=>{
+//            val list=str.split(" ")
+//        })
+    }
+}
